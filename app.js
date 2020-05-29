608235bce35df317b6669238a7c9f22d8be34883
Harvinder Singh <43422594+harvindersingh011@users.noreply.github.com>
2020-05-28 21:41:37 -0400

    FIRST COMMIT

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+    res.end('Hello World!');
+}).listen(3000);
+
+
+console.log('Hello world ');
